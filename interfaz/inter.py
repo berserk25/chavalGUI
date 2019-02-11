9687c443058ee1578c1187e2d3fd1dec84cd427e
berserk25 <abi25programer@outlook.com>
2019-02-10 18:27:11 -0600

    pequeño inicio de codigo chaval

---
+++ interfaz/inter.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import*
+import sqlite3
+
+ventana = Tk()
+ventana.mainloop()
+
+class FirstWindow:
+	def __init__(self, ventana):
+		self.confi_ventana = ventana
+		self.confi_ventana.title("Principal Window Application")
+
+
+
+
+
